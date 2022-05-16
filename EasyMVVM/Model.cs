6151fcb545f54c790d9b6bfcbb2707322c52852b
alexrockss <104759110+alexrockss@users.noreply.github.com>
2022-05-16 14:19:40 +0300

    ex

---
+++ EasyMVVM/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMVVM
+{
+    public class Model
+    {
+        readonly ObservableCollection<string> _data = new();
+        public ObservableCollection<string> GetData()
+        {
+            _data.Add("First Entry");
+            _data.Add("Second Entry");
+            _data.Add("Third Entry");
+            return _data;
+        }
+    }
+}

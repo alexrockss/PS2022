dcd9092b95e51a5665fb0f0bbc5ee806359db274
alexrockss <104759110+alexrockss@users.noreply.github.com>
2022-05-16 14:22:35 +0300

    Add files via upload

---
+++ WPFHello/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WPFHello;
+
+namespace WPFhello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListBoxItem james = new();
+            james.Content = "James";
+
+            ListBoxItem david = new();
+            david.Content = "David";
+
+            peopleListBox.Items.Add(james);
+            peopleListBox.Items.Add(david);
+
+            peopleListBox.SelectedItem = james;
+        }
+
+        private void OnClickHello(object sender, RoutedEventArgs e)
+        {
+            if (txtName.Text.Length <= 1)
+            {
+                MessageBox.Show("Грешни данни, опитай отново");
+            }
+            else
+            {
+                MessageBox.Show("Здрасти " + txtName.Text + "!\n Това е твоята първа програма! ");
+            }
+        }
+        private void BtnGreedClick(object sender, RoutedEventArgs e)
+        {
+            string greetingMsg;
+            greetingMsg = (peopleListBox.SelectedItem as ListBoxItem).Content.ToString();
+            MessageBox.Show("Hi " + greetingMsg);
+        }
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+
+            MyMessage anotherWindow = new();
+            anotherWindow.Show();
+
+        }
+    }
+}
